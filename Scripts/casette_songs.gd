0b44c1a55fede55e29bbd7d4ada7501837269bf2
AeroSpace <aerospace637@gmail.com>
2026-06-01 19:22:12 -0500

    Casette State 3

--- Scripts/casette_songs.gd
+++ Scripts/casette_songs.gd
@@ -4,6 +4,8 @@ var group_scene = preload("res://Scenes/song_group.tscn")
 
 @onready var group_container: VBoxContainer = %SongsGroup
 @onready var current_song: Control = %CurrentSong
+@onready var song_selection: OptionButton = %SongSelection
+@onready var add_remove: Button = $AddRemove
 
 var recent_casette: Casette = null
 
@@ -13,6 +15,7 @@ func _ready() -> void:
 func open_casette(res: Casette):
 	sort_songs(res.song_indexes)
 	recent_casette = res
+	load_new_songs()
 	show()
 
 func close_casette():
@@ -35,6 +38,30 @@ func sort_songs(indexes: Array):
 		new_song.connect("casette_me", TrackManager.send_casette)
 		new_song.connect("casette_me", _set_current)
 		group_container.add_child(new_song)
+
+func load_new_songs():
+	song_selection.clear()
+	
+	for i in AudioManager.song_library.songs_list.values():
+		song_selection.add_item(i.name)
+
+func song_selected(_idx: int):
+	if !song_selection.selected in recent_casette.song_indexes:
+		add_remove.text = "Add"
+	else:
+		add_remove.text = "Remove"
+
+func add_or_remove():
+	var idx: int = song_selection.get_item_index(song_selection.get_selected_id())
+	if idx in recent_casette.song_indexes:
+		PlaylistManager.collection_library.casette_list[recent_casette.my_name].song_indexes.erase(idx)
+	else:
+		PlaylistManager.collection_library.casette_list[recent_casette.my_name].song_indexes.append(idx)
+	
+	recent_casette.song_indexes = PlaylistManager.collection_library.casette_list[recent_casette.my_name].song_indexes
+	sort_songs(PlaylistManager.collection_library.casette_list[recent_casette.my_name].song_indexes)
+	song_selected(-1)
+
 
 func _set_current(nm: String, auth: String):
 	PlaylistManager.selected_casette = recent_casette.my_name
